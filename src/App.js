2c4dbe5c6bfd3da024f621ed3e4e1d36f2af922a
unknown <shreedhar7bpatil@gmail.com>
2022-07-29 23:37:57 +0530

    added tailwind dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        
-      </header>
-    </div>
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
   )
 }
 
